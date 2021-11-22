5b34ef436b103e6e73dee57663787cb49ee6e447
pokorra <patrycja.pokora@gmail.com>
2021-11-22 21:28:13 +0100

    days in random order

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -9,33 +9,33 @@ const Container = () => {
   return (
     <div className="main-container">
       {/* <div className="main-container__row"> */}
-        <Window currentDate={currentDate} thisDay={1} />
-        <Window currentDate={currentDate} thisDay={2} />
-        <Window currentDate={currentDate} thisDay={3} />
-        <Window currentDate={currentDate} thisDay={4} />
         <Window currentDate={currentDate} thisDay={5} />
-        <Window currentDate={currentDate} thisDay={6} />
-        <Window currentDate={currentDate} thisDay={7} />
-        <Window currentDate={currentDate} thisDay={8} />
-      {/* </div> */}
-      {/* <div className="main-container__row"> */}
-        <Window currentDate={currentDate} thisDay={9} />
-        <Window currentDate={currentDate} thisDay={10} />
-        <Window currentDate={currentDate} thisDay={11} />
-        <Window currentDate={currentDate} thisDay={12} />
-        <Window currentDate={currentDate} thisDay={13} />
-        <Window currentDate={currentDate} thisDay={14} />
-        <Window currentDate={currentDate} thisDay={15} />
-        <Window currentDate={currentDate} thisDay={16} />
-      {/* </div> */}
-      {/* <div className="main-container__row"> */}
-        <Window currentDate={currentDate} thisDay={17} />
-        <Window currentDate={currentDate} thisDay={18} />
-        <Window currentDate={currentDate} thisDay={19} />
-        <Window currentDate={currentDate} thisDay={20} />
         <Window currentDate={currentDate} thisDay={21} />
-        <Window currentDate={currentDate} thisDay={22} />
+        <Window currentDate={currentDate} thisDay={19} />
+        <Window currentDate={currentDate} thisDay={17} />
+        <Window currentDate={currentDate} thisDay={1} />
         <Window currentDate={currentDate} thisDay={23} />
+        <Window currentDate={currentDate} thisDay={2} />
+        <Window currentDate={currentDate} thisDay={15} />
+      {/* </div> */}
+      {/* <div className="main-container__row"> */}
+        <Window currentDate={currentDate} thisDay={10} />
+        <Window currentDate={currentDate} thisDay={3} />
+        <Window currentDate={currentDate} thisDay={9} />
+        <Window currentDate={currentDate} thisDay={7} />
+        <Window currentDate={currentDate} thisDay={12} />
+        <Window currentDate={currentDate} thisDay={20} />
+        <Window currentDate={currentDate} thisDay={13} />
+        <Window currentDate={currentDate} thisDay={6} />
+      {/* </div> */}
+      {/* <div className="main-container__row"> */}
+        <Window currentDate={currentDate} thisDay={14} />
+        <Window currentDate={currentDate} thisDay={22} />
+        <Window currentDate={currentDate} thisDay={18} />
+        <Window currentDate={currentDate} thisDay={11} />
+        <Window currentDate={currentDate} thisDay={16} />
+        <Window currentDate={currentDate} thisDay={4} />
+        <Window currentDate={currentDate} thisDay={8} />
         <Window currentDate={currentDate} thisDay={24} />
       {/* </div> */}
     </div>

e54d79757f429dce63788ae86fa26ed591275525
pokorra <patrycja.pokora@gmail.com>
2021-11-30 22:08:52 +0100

    database complemented with first ten days

--- src/dataBase/dataBase.js
+++ src/dataBase/dataBase.js
@@ -1,5 +1,8 @@
 import konkurs_mis_pracus from "./files/konkurs_mis_pracus.jpg";
 import pobranie_Wesole_swieta from "./files/pobranie_Wesole_swieta.pdf";
+import kalendarz_adwent_emi from "./files/kalendarz_adwent_emi.pdf";
+import EMI_ida_swieta_do_pobrania from "./files/EMI_ida_swieta_do_pobrania.pdf";
+import konkurs_ania from "./files/konkurs_ania.jpg";
 
 export const arrayOfGifts = [
   <div className="gift-container">
@@ -17,10 +20,18 @@ export const arrayOfGifts = [
       target="_blank"
       rel="noreferrer"
       download={pobranie_Wesole_swieta}
-    > <p className="gift-anchor-content"> "wesołe święta"</p></a>
+    >
+      <p className="gift-anchor-content">wesołe święta</p>
+    </a>
   </div>,
   <div className="gift-container">
-    <div>DZIEŃ 3 DZIKIE DZIECI</div>
+    <iframe
+      src="https://www.youtube.com/embed/dkgNQpeBy_g"
+      title="Jak wykonać drewniane ozdoby"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
   </div>,
   <div className="gift-container">
     <img
@@ -29,12 +40,56 @@ export const arrayOfGifts = [
       className="gift-container__image"
     ></img>
   </div>,
-  <div className="gift-container"></div>,
-  <div className="gift-container"></div>,
-  <div className="gift-container"></div>,
-  <div className="gift-container"></div>,
-  <div className="gift-container"></div>,
-  <div className="gift-container"></div>,
+  <div className="gift-container gift-container__primary gift-download gift-emi">
+    <a
+      href={kalendarz_adwent_emi}
+      target="_blank"
+      rel="noreferrer"
+      download={kalendarz_adwent_emi}
+    >
+      <p className="gift-anchor-content">
+        fragment książki o przygodach Emi w Szwecji
+      </p>
+    </a>
+  </div>,
+  <div className="gift-container gift-display">
+    <div className="gift-container__display gift-mikolajki"></div>
+  </div>,
+  <div className="gift-container">
+    <iframe
+      src="https://www.youtube.com/embed/QaxzGy3I4Lo"
+      title="Marcin Przewoźniak czyta Mamę w occie"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
+  </div>,
+  <div className="gift-container">
+    <iframe
+      src="https://www.youtube.com/embed/Vp-KdQ_DlnU"
+      title="Jak zrobić kartkę z bałwankiem"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
+  </div>,
+  <div className="gift-container gift-container__primary gift-download gift-emi-download">
+    <a
+      href={EMI_ida_swieta_do_pobrania}
+      target="_blank"
+      rel="noreferrer"
+      download={EMI_ida_swieta_do_pobrania}
+    >
+      <p className="gift-anchor-content">Emi - materiały do pobrania</p>
+    </a>
+  </div>,
+  <div className="gift-container">
+    <img
+      src={konkurs_ania}
+      alt="konkurs: Ania z Zielonego Wzgórza"
+      className="gift-container__image"
+    ></img>
+  </div>,
   <div className="gift-container"></div>,
   <div className="gift-container"></div>,
   <div className="gift-container"></div>,
